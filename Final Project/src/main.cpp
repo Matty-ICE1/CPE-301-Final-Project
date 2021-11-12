42a3c084869558704ea9284a06ec53379aa7453e
bredbord <larsriver@yahoo.com>
2021-11-12 12:10:15 -0800

    PIO and Water Sensor

---
+++ Final Project/src/main.cpp
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+#include "readAnalog.h"
+
+#define WATER_PIN 0
+#define WATER_CUTOFF 100
+
+// Globals---------------------------------------------
+
+// Function Protoypes----------------------------------
+unsigned char getWaterLevel();
+
+void setup() {
+  startADC();  // start the ADC
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println(getWaterLevel());
+  delay(500);
+}
+
+// Additional Functions--------------------------------
+unsigned char getWaterLevel() {
+  unsigned int reading = readAnalog(WATER_PIN);
+  if (reading < WATER_CUTOFF) return 0;
+  return map(reading, WATER_CUTOFF, 500, 1, 100);
+}
---
+++ Final Project/src/readAnalog.h
@@ -0,0 +1,60 @@
+#ifndef ANALOG_READ_H
+#define ANALOG_READ_H
+
+volatile unsigned char* my_ADMUX = (unsigned char*) 0x7C;
+volatile unsigned char* my_ADCSRB = (unsigned char*) 0x7B;
+volatile unsigned char* my_ADCSRA = (unsigned char*) 0x7A;
+volatile unsigned int* my_ADC_DATA = (unsigned short*) 0x78;
+
+void startADC()
+{
+  // set up the A register
+  *my_ADCSRA |= 0b10000000; // set bit   7 to 1 to enable the ADC
+  *my_ADCSRA &= 0b11011111; // clear bit 5 to 0 to disable the ADC trigger mode
+  *my_ADCSRA &= 0b11110111; // clear bit 3 to 0 to disable the ADC interrupt
+  *my_ADCSRA &= 0b11111000; // clear bit 2-0 to 0 to set prescaler selection to slow reading
+     
+  // set up the B register
+  *my_ADCSRB &= 0b11110111; // clear bit 3 to 0 to reset the channel and gain bits
+  *my_ADCSRB &= 0b11111000; // clear bit 2-0 to 0 to set free running mode
+  
+  // set up the MUX Register
+  *my_ADMUX  &= 0b01111111; // clear bit 7 to 0 for AVCC analog reference
+  *my_ADMUX  |= 0b01000000; // set bit   6 to 1 for AVCC analog reference
+  *my_ADMUX  &= 0b11011111; // clear bit 5 to 0 for right adjust result
+  *my_ADMUX  &= 0b11101111; // clear bit 4-0 to 0 to reset the channel and gain bits
+}
+
+unsigned int readAnalog(unsigned char adc_channel_num)
+{
+  // reset the channel and gain bits
+  *my_ADMUX  &= 0b11100000;
+  
+  // clear the channel selection bits
+  *my_ADCSRB &= 0b11110111;
+  
+  // set the channel number
+  if(adc_channel_num > 7)
+  {
+    // set the channel selection bits, but remove the most significant bit (bit 3)
+    adc_channel_num -= 8;
+    
+    // set MUX bit 
+    *my_ADCSRB |= 0b00001000;
+  }
+  
+  // set the channel selection bits
+  *my_ADMUX  += adc_channel_num;
+  
+  // set bit ?? of ADCSRA to 1 to start a conversion
+  *my_ADCSRA |= 0b01000000;
+  
+  // wait for the conversion to complete
+  while((*my_ADCSRA & 0x40) !=0);
+  
+  // return the result in the ADC data register
+  return *my_ADC_DATA;
+}
+
+
+#endif
