6b2d7d45892061193053834617c66ebeefa71555
bredbord <larsriver@yahoo.com>
2021-11-17 00:13:33 -0800

    LED foundation in place

---
+++ Final Project/src/LedWrite.h
@@ -0,0 +1,4 @@
+#ifndef LED_WRITE_H
+#define LED_WRITE_H
+
+#endif
--- Final Project/src/main.cpp
+++ Final Project/src/main.cpp
@@ -4,25 +4,52 @@
 #include <LiquidCrystal.h>
 #include "readAnalog.h"
 
+//PORT B LED Configuration
+#define RED 7
+#define BLUE 6
+#define GREEN 5
+#define YELLOW 4
+
+//Water Sensor Configuration
 #define WATER_PIN 0  // A0
 #define WATER_CUTOFF 100
 
+//Servo Motor Configuration
 #define SERVO_PIN 6
 #define SERVO_KNOB 1  // A1
 
+// Temperature Configuration
+#define TEMP_THRESHOLD 25 // *C
+
+#define RS 22
+#define EN 23
+#define D4 24
+#define D5 25
+#define D6 26
+#define D7 27
+
 // Globals---------------------------------------------
 bool isDisabled = false;
-unsigned char status = 0;
+char status;
 Servo vent;
-LiquidCrystal lcd(8,9, 4,5,6,7); // rs, enable, d4, d5, d6, d7
+LiquidCrystal lcd(RS, EN, D4, D5, D6, D7); // rs, enable, d4, d5, d6, d7
 
 // Function Protoypes----------------------------------
 unsigned char getWaterLevel();
 void runLCD(unsigned char);
+void digitalWriteLED(unsigned char, bool);
+float getTemperature();
 
+
+//MAIN=================================================
 void setup() {
+  //Output pins
+  DDRB |= 0b00001111; // PB 4,5,6,7
+
+  //ADC
   startADC();  // start the ADC
-  Serial.begin(9600);
+
+  //Servo
   vent.attach(SERVO_PIN);
 
   //LCD Setup
@@ -31,8 +58,16 @@ void setup() {
 }
 
 void loop() {
-  vent.write(map(readAnalog(SERVO_KNOB), 2, 894, 0, 100));
-  runLCD(status);
+  if(!isDisabled) {
+    if (getWaterLevel() < 0) status = 'E';  // Disabled
+    else status = 'I'; // Idle
+
+    if (getTemperature() > TEMP_THRESHOLD) status = 'R';
+    else status = 'I';
+  }
+  else {
+    //yellow LED
+  }
 }
 
 // Additional Functions--------------------------------
@@ -43,7 +78,34 @@ unsigned char getWaterLevel() {
 }
 
 void runLCD(unsigned char s) {
-  if (s== 0) {
-    lcd.print("Status:");
-  }
+}
+
+void digitalWriteLED(unsigned char l, bool state) {
+    switch(l) {
+        case 'r':  // MEGA pin 50
+            l = 0b00001000;
+            break;
+
+        case 'y':  // MEGA pin 51
+            l = 0b00000100;
+            break;
+
+        case 'g':  // MEGA pin 52
+            l = 0b00000010;
+            break;
+
+        case 'b':  // MEGA pin 53
+            l = 0b00000001;
+            break;
+
+        default:
+            break;
+    }
+
+    if (state) PORTB |= l;
+    else PORTB &= ~l;
+}
+
+float getTemperature(){
+  return 0.0;
 }
